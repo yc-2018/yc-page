80e56a8f389af308852fcf6fe242b50eb558851b
YC2023 <cgl@88.com>
2023-12-05 02:33:08 +0800

    文本间加个空格

--- src/main/java/ikun/yc/ycpage/service/impl/WechatServiceImpl.java
+++ src/main/java/ikun/yc/ycpage/service/impl/WechatServiceImpl.java
@@ -25,6 +25,6 @@ public class WechatServiceImpl implements WechatService {
         } while (Boolean.TRUE.equals(redisTemplate.hasKey(code)));   // 直接检查验证码是否已作为键存在
 
         redisTemplate.opsForValue().set(code, toUserName,60, TimeUnit.MINUTES);          // 存储验证码和用户名的映射
-        return "登录验证码为:" + code + ",5分钟内有效。\n失效后可重新发送登录获取验证码,目前网站地址:https://yc556.gitee.io";
+        return "登录验证码为:" + code + ", 5分钟内有效。\n失效后可重新发送登录获取验证码,目前网站地址:https://yc556.gitee.io";
     }
 }
