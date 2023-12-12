9d1c06711d2099c478f9309c46f957e6186ae864
仰晨 <cgl556@88.com>
2023-12-12 22:41:30 +0800

    Swagger文档也改为3，然后还要在配置类里面加点配置才能加token

--- src/main/java/ikun/yc/ycpage/config/SwaggerConfig.java
+++ src/main/java/ikun/yc/ycpage/config/SwaggerConfig.java
@@ -5,9 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * swagger3默认首页变成 <a href="http://localhost:8080/swagger-ui/index.html">swagger3</a>
@@ -17,12 +22,14 @@ public class SwaggerConfig {
   
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
+    return new Docket(DocumentationType.OAS_30)
       .apiInfo(apiInfo())
       .select()
       .apis(RequestHandlerSelectors.basePackage("ikun.yc.ycpage.controller"))
       .paths(PathSelectors.any())
-      .build();
+      .build()//;
+      .securitySchemes(securitySchemes())
+      .securityContexts(Collections.singletonList(securityContext()));
   }
 
 
@@ -33,5 +40,32 @@ public class SwaggerConfig {
             .description("仰晨主页接口文档")
             .build();
   }
+
+
+  private List<SecurityScheme> securitySchemes() {
+    List<SecurityScheme> apiKeyList = new ArrayList<>();
+    //注意，这里应对应登录token鉴权对应的k-v
+    apiKeyList.add(new ApiKey("Authorization", "Authorization", "header"));
+    return apiKeyList;
+  }
+
+  /**
+   * 这里设置 swagger2 认证的安全上下文
+   */
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+            .securityReferences(Collections.singletonList(new SecurityReference("Authorization", scopes())))
+            .build();
+  }
+
+  /**
+   * 这里是写允许认证的scope
+   */
+  private AuthorizationScope[] scopes() {
+    return new AuthorizationScope[]{
+            new AuthorizationScope("web", "All scope is trusted!")
+    };
+  }
+
 
 }
