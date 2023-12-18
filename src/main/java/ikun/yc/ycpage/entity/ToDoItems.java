30d6df33bad2452bc52d6f3b1c0a0c3d7c41c42e
仰晨 <cgl556@88.com>
2023-12-18 21:01:43 +0800

    修改为现在时间注解不生效，搞了半天还换了一个最新的MyBatis-Plus依赖。最后发现。加上fill = FieldFill.UPDATE就可以了，但是在公司里面测试吗过好像不行，我也不知道为什么。

--- src/main/java/ikun/yc/ycpage/entity/ToDoItems.java
+++ src/main/java/ikun/yc/ycpage/entity/ToDoItems.java
@@ -1,14 +1,12 @@
 package ikun.yc.ycpage.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+
 
 /**
  * (to_do_items)实体类
@@ -46,7 +44,7 @@ public class ToDoItems{
     private LocalDateTime createTime;
 
     /** 修改时间 */
-    @TableField(update = "now()")
+    @TableField(fill = FieldFill.UPDATE, update = "now()")
 	private LocalDateTime updateTime;
 
     public ToDoItems(String userId, String content, Integer itemType) {
