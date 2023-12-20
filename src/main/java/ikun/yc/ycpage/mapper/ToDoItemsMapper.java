2ed0f84ab57aff4bc7ef290e3f33843be3cbf1f1
仰晨 <cgl556@88.com>
2023-12-20 18:15:40 +0800

    分组统计加在标签上面 未完成的条数。但是不包括 2长期、1循环、4英语、5日记、和当前的

--- src/main/java/ikun/yc/ycpage/mapper/ToDoItemsMapper.java
+++ src/main/java/ikun/yc/ycpage/mapper/ToDoItemsMapper.java
@@ -17,7 +17,10 @@ import java.util.Map;
 @Mapper
 public interface ToDoItemsMapper extends BaseMapper<ToDoItems> {
 
-    // 分组统计加在标签上面 未完成的条数。
-    @Select("SELECT item_type,count(*) FROM to_do_items WHERE user_id=#{userId} AND completed=0 AND item_type NOT IN (4, #{itemType}) group by item_type")
+    // 分组统计加在标签上面 未完成的条数。但是不包括 2长期、1循环、4英语、5日记、和当前的
+    @Select("SELECT item_type,count(*) " +
+            "FROM to_do_items " +
+            "WHERE user_id=#{userId} AND completed=0 AND item_type NOT IN (1, 2, 4, 5, #{itemType}) " +
+            "group by item_type")
     List<Map> selectGroupToDoItemsCount(ToDoItems toDoItems);
 }
