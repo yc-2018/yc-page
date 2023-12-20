f429f68a57f24cfb3c5dde1a525fe9cbdb1bd494
仰晨 <cgl556@88.com>
2023-12-20 09:12:43 +0800

    报错了

--- src/main/java/ikun/yc/ycpage/mapper/ToDoItemsMapper.java
+++ src/main/java/ikun/yc/ycpage/mapper/ToDoItemsMapper.java
@@ -15,7 +15,7 @@ import java.util.Map;
  * @since 2023-12-03 22:31:22
 */
 @Mapper
-public interface ToDoItemsMapper extends BaseMapper<ToDoItems> {
+public interface ToDoItemsMapper extends BaseMapper<Object> {
 
     // 分组统计加在标签上面 未完成的条数。
     @Select("SELECT item_type,count(*) FROM to_do_items WHERE user_id=#{userId} AND completed=0 AND item_type NOT IN (4, #{type}) group by item_type")
