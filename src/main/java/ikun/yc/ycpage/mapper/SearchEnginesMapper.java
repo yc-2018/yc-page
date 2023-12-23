9e6fbdc36279a548766473cdce984cfb2f643016
仰晨 <cgl556@88.com>
2023-12-23 21:03:37 +0800

    还得求教朱哥合并分支

--- src/main/java/ikun/yc/ycpage/mapper/SearchEnginesMapper.java
+++ src/main/java/ikun/yc/ycpage/mapper/SearchEnginesMapper.java
@@ -1,16 +1,20 @@
 package ikun.yc.ycpage.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import ikun.yc.ycpage.entity.SearchEngines;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * (search_engines)数据Mapper
  *
  * @author yc
  * @since 2023-12-03 22:31:22
+ * @description 由 Mybatisplus Code Generator 创建
 */
 @Mapper
 public interface SearchEnginesMapper extends BaseMapper<SearchEngines> {
+    void batchUpdateSearchEngines(List<SearchEngines> searchEngineList);
 
 }
