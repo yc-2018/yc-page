77d14610f678d214663d5f105a56b3779d31f22f
仰晨 <cgl556@88.com>
2023-12-23 16:27:27 +0800

    回到最初的起点

--- src/main/java/ikun/yc/ycpage/controller/SearchEnginesController.java
+++ src/main/java/ikun/yc/ycpage/controller/SearchEnginesController.java
@@ -35,28 +35,26 @@ public class SearchEnginesController {
     public R<Boolean> addSearchEngines(@RequestBody SearchEngines searchEngines) {
         if (searchEngines.getEngineUrl()== null) return R.error("URL不允许为空");
         if (searchEngines.getName()== null) return R.error("名称不允许为空");
-        if (searchEngines.getIsQuickSearch()== null) return R.error("引擎类型不允许为空");
 
         searchEngines.setUserId(BaseContext.getCurrentId());
         return R.success(searchEnginesService.save(searchEngines));
     }
 
     @PutMapping
-    public R<Boolean> updateSearchEngines(@RequestBody List<SearchEngines> searchEngineList) {
+    public R<?> updateSearchEngines(@RequestBody List<SearchEngines> searchEngineList) {
         if (searchEngineList == null) return R.error("ID不允许为空");
-
-//        LambdaUpdateWrapper<SearchEngines> wrapper = new LambdaUpdateWrapper<>();
-//        for (SearchEngines searchEngines : searchEngineList) {
-//            wrapper .or()
-//                    .eq(SearchEngines::getId, searchEngines.getId())
-//                    .eq(SearchEngines::getUserId, BaseContext.getCurrentId())
-//                    .set(searchEngines.getEngineUrl() != null, SearchEngines::getEngineUrl, searchEngines.getEngineUrl())
-//                    .set(searchEngines.getName() != null, SearchEngines::getName, searchEngines.getName())
-//                    .set(searchEngines.getIconUrl() != null, SearchEngines::getIconUrl, searchEngines.getIconUrl())
-//                    .set(searchEngines.getIsQuickSearch() != null, SearchEngines::getIsQuickSearch, searchEngines.getIsQuickSearch());
-//        }
-        searchEnginesService.batchUpdate(searchEngineList);
-        return R.success(true);
+        StringBuilder sb = new StringBuilder();
+        for (SearchEngines searchEngines : searchEngineList) {
+            sb.append( searchEnginesService.update(new LambdaUpdateWrapper<SearchEngines>()
+                    .eq(SearchEngines::getId, searchEngines.getId())
+                    .eq(SearchEngines::getUserId, BaseContext.getCurrentId())
+                    .set(searchEngines.getEngineUrl() != null, SearchEngines::getEngineUrl, searchEngines.getEngineUrl())
+                    .set(searchEngines.getName() != null, SearchEngines::getName, searchEngines.getName())
+                    .set(searchEngines.getIconUrl() != null, SearchEngines::getIconUrl, searchEngines.getIconUrl())
+                    .set(searchEngines.getIsQuickSearch() != null, SearchEngines::getIsQuickSearch, searchEngines.getIsQuickSearch())
+            ));
+        }
+        return R.success(sb);
     }
 
     @DeleteMapping
