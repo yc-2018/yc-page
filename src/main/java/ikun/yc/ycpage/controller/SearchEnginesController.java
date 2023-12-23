046610195baa73b13a408867a35fecd315c8b9d5
仰晨 <cgl556@88.com>
2023-12-23 21:00:42 +0800

    还得求教朱哥合并分支

--- src/main/java/ikun/yc/ycpage/controller/SearchEnginesController.java
+++ src/main/java/ikun/yc/ycpage/controller/SearchEnginesController.java
@@ -1,7 +1,6 @@
 package ikun.yc.ycpage.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import ikun.yc.ycpage.common.BaseContext;
 import ikun.yc.ycpage.common.R;
 import ikun.yc.ycpage.entity.SearchEngines;
@@ -26,11 +25,27 @@ import java.util.List;
 public class SearchEnginesController {
     private final SearchEnginesService searchEnginesService;
 
+    /**
+     * 按用户id获取列表
+     *
+     * @return 搜索引擎列表
+     * @author ChenGuangLong
+     * @since 2023/12/23 16:53:55
+     */
     @GetMapping("/list")
     public R<List<SearchEngines>> getListByUserId() {
         return R.success(searchEnginesService.listByMap(Collections.singletonMap("user_id", BaseContext.getCurrentId())));
     }
 
+
+    /**
+     * 添加搜索引擎
+     *
+     * @param searchEngines 搜索引擎
+     * @return 成功与否
+     * @author ChenGuangLong
+     * @since 2023/12/23 16:53:03
+     */
     @PostMapping
     public R<Boolean> addSearchEngines(@RequestBody SearchEngines searchEngines) {
         if (searchEngines.getEngineUrl()== null) return R.error("URL不允许为空");
@@ -41,9 +56,18 @@ public class SearchEnginesController {
         return R.success(searchEnginesService.save(searchEngines));
     }
 
+
+    /**
+     * 批量循环更新搜索引擎
+     *
+     * @param searchEngineList 搜索引擎列表
+     * @return 返回拼接的循环进去的true或false
+     * @author ChenGuangLong
+     * @since 2023/12/23
+     */
     @PutMapping
-    public R<Boolean> updateSearchEngines(@RequestBody List<SearchEngines> searchEngineList) {
-        if (searchEngineList == null) return R.error("ID不允许为空");
+    public R<?> updateSearchEngines(@RequestBody List<SearchEngines> searchEngineList) {
+        if (searchEngineList == null || searchEngineList.size() == 0) return R.error("乱搞！🤺");
 
 //        LambdaUpdateWrapper<SearchEngines> wrapper = new LambdaUpdateWrapper<>();
 //        for (SearchEngines searchEngines : searchEngineList) {
@@ -59,6 +83,15 @@ public class SearchEnginesController {
         return R.success(true);
     }
 
+
+    /**
+     * 批量删除搜索引擎
+     *
+     * @param ids 引擎id
+     * @return 成功返回true，失败返回提示信息
+     * @author ChenGuangLong
+     * @since 2023/12/23
+     */
     @DeleteMapping
     public R<Boolean> deleteSearchEngines(@RequestBody List<Integer> ids) {
         LambdaQueryWrapper<SearchEngines> wrapper = new LambdaQueryWrapper<>();
