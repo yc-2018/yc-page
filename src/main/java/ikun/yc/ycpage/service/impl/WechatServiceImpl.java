fda2411223c8f7defd9ee50cdd746a1e47dd05a8
仰晨 <cgl556@88.com>
2024-01-20 21:31:18 +0800

    md 之前登录验证码写的是5分钟失效 写成了一个小时失效 现在改成1分钟失效

--- src/main/java/ikun/yc/ycpage/service/impl/WechatServiceImpl.java
+++ src/main/java/ikun/yc/ycpage/service/impl/WechatServiceImpl.java
@@ -38,8 +38,8 @@ public class WechatServiceImpl implements WechatService {
             code = VerificationCodeUtil.generateCode();
         } while (Boolean.TRUE.equals(redisTemplate.hasKey(code)));   // 直接检查验证码是否已作为键存在
 
-        redisTemplate.opsForValue().set(code, toUserName,60, TimeUnit.MINUTES);          // 存储验证码和用户名的映射
-        return "登录验证码为:" + code + "，五分钟内有效。\n失效后可重新发送登录获取验证码,网站地址:https://yc556.cn";
+        redisTemplate.opsForValue().set(code, toUserName,1, TimeUnit.MINUTES);          // 存储验证码和用户名的映射
+        return "登录验证码为:" + code + "，一分钟内有效。\n失效后可重新发送登录获取验证码,网站地址:https://yc556.cn";
     }
 
 
