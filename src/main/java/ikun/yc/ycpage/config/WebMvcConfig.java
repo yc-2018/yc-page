c117464bc63f566b429f45186ca06b2e2e255e8d
仰晨 <cgl556@88.com>
2025-03-17 11:05:23 +0800

    refactor: 优化时间格式序列

--- src/main/java/ikun/yc/ycpage/config/WebMvcConfig.java
+++ src/main/java/ikun/yc/ycpage/config/WebMvcConfig.java
@@ -78,6 +78,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
      * 定义全局默认时间序列化
      * 配置完后，格式如 "createTime": "2023-12-05 20:43:20",
      * 没配置时，格式如 "createTime": [2023,12,11,9,25,12]
+     * <br/>
+     * 如果需要 支持多格式解析（deepSeek 未试）
+     * 1️⃣DateTimeFormatter formatter = DateTimeFormatter.ofPattern("[yyyy-MM-dd HH:mm:ss][yyyy-MM-dd'T'HH:mm:ss]");
+     * .deserializers(new LocalDateTimeDeserializer(formatter))
+     * 2️⃣在 application.properties 中启用宽松日期解析
+     * spring.jackson.deserialization.lenient=true
      */
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
