023b07cbdcaac6d82aff6eaf78594147494b483a
仰晨 <cgl556@88.com>
2026-04-17 00:30:54 +0800

    feat(mini-account-memo): 添加图片字段支持并优化时间戳更新
    - 在更新操作中添加了图片字段的条件设置功能
    - 将日期时间类型从 LocalDate 统一更新为 LocalDateTime
    - 优化了实体更新逻辑以支持图片字段的动态更新
    - 统一了删除操作中的时间戳更新机制

--- src/main/java/ikun/yc/ycpage/controller/MiniAccountMemoController.java
+++ src/main/java/ikun/yc/ycpage/controller/MiniAccountMemoController.java
@@ -12,14 +12,9 @@ import ikun.yc.ycpage.entity.MiniAccountMemo;
 import ikun.yc.ycpage.service.MiniAccountMemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
@@ -85,7 +80,8 @@ public class MiniAccountMemoController {
                 .set(StringUtils.hasText(miniAccountMemo.getAccount()), MiniAccountMemo::getAccount, miniAccountMemo.getAccount())
                 .set(StringUtils.hasText(miniAccountMemo.getPassword()), MiniAccountMemo::getPassword, miniAccountMemo.getPassword())
                 .set(MiniAccountMemo::getRemark, miniAccountMemo.getRemark())
-                .set(MiniAccountMemo::getUpdatedAt, LocalDate.now())
+                .set(StringUtils.hasText(miniAccountMemo.getImgs()), MiniAccountMemo::getImgs, miniAccountMemo.getImgs())
+                .set(MiniAccountMemo::getUpdatedAt, LocalDateTime.now())
         );
         return updateOk ? R.success(true) : R.error("修改失败");
     }
@@ -98,7 +94,7 @@ public class MiniAccountMemoController {
                 .eq(MiniAccountMemo::getUserOpenid, BaseContext.getCurrentId())
                 .eq(MiniAccountMemo::getIsDeleted, 0)
                 .set(MiniAccountMemo::getIsDeleted, 1)
-                .set(MiniAccountMemo::getUpdatedAt, LocalDate.now())
+                .set(MiniAccountMemo::getUpdatedAt, LocalDateTime.now())
         );
         return updateOk ? R.success(true) : R.error("删除失败");
     }
